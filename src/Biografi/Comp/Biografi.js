8ecac02a78aae2f9c915fa815d5b336c5b08b46c
fahmi3232 <fahimun32@gmail.com>
2022-01-17 16:52:59 -0800

    Update tugas styling week 6 - React JS

--- src/Biografi/Comp/Biografi.js
+++ src/Biografi/Comp/Biografi.js
@@ -1,8 +1,27 @@
 import React from "react";
 import { Container, Row, Col } from "react-bootstrap";
-import SubBio from "./SubBio";
+// import SubBio from "./SubBio";
+import { useState } from "react";
+import Media from '../Images/media-sosial.png'
 
 const Biografi = () => {
+  const [bio, setBio] = useState([
+    {
+      title: "Front End",
+      deskripsi:
+        "Lorem ipsum dolor, sit amet consectetur adipisicing elit. Natusanimi quae odit nesciunt quasi impedit ea pariatur",
+    },
+    {
+      title: "Back End",
+      deskripsi:
+        "Lorem ipsum dolor, sit amet consectetur adipisicing elit. Natusanimi quae odit nesciunt quasi impedit ea pariatur",
+    },
+    {
+      title: "Framework",
+      deskripsi:
+        "Lorem ipsum dolor, sit amet consectetur adipisicing elit. Natusanimi quae odit nesciunt quasi impedit ea pariatur",
+    },
+  ]);
   return (
     <div className="mc">
       <Container className="pt-5 pb-3">
@@ -20,27 +39,13 @@ const Biografi = () => {
       </Container>
       <Container className="pb-5 text-center">
         <Row>
-          <Col md={4}>
-            <SubBio
-              title="Front End"
-              deskripsi="Lorem ipsum dolor, sit amet consectetur adipisicing elit. Natus
-              animi quae odit nesciunt quasi impedit ea pariatur"
-            />
-          </Col>
-          <Col md={4}>
-            <SubBio
-              title="Back End"
-              deskripsi="Lorem ipsum dolor, sit amet consectetur adipisicing elit. Natus
-              animi quae odit nesciunt quasi impedit ea pariatur"
-            />
-          </Col>
-          <Col md={4}>
-            <SubBio
-              title="Framework"
-              deskripsi="Lorem ipsum dolor, sit amet consectetur adipisicing elit. Natus
-              animi quae odit nesciunt quasi impedit ea pariatur"
-            />
-          </Col>
+          {bio.map((biograf) => (
+            <Col style={{width: 345}} className="sb mx-3" md={4}>
+              <img src={Media} alt="" />
+              <h2>{biograf.title}</h2>
+              <p>{biograf.deskripsi}</p>
+            </Col>
+          ))}
         </Row>
       </Container>
     </div>
